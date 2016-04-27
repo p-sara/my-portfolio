8a22bcab4fe68ffe2481ab17fe8360f6fc2d1d75
sarusia00 <sarus00@gmail.com>
2016-04-27 16:23:57 +0100

    working on header slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,74 @@
 $(function() {
-   $(".web").mouseenter(function() {
-     $($(this)).next().css("display", "inline-block");
+ //slider
+  var left = $(".left");
+  var right = $(".right");
+  var photo = $(".photo");
+  var slider = $(".background-slider");
+  var window = $(".window");
+  var photoWidth = photo.width();
+  console.log(slider.position().left);
+
+  slider.css({
+    width: "30000px",
+  });
+  var offset = 0;
+ var newSlider = slider.clone(true);
+ newSlider.css({
+   display: "inline-block",
+   position: "relative",
+ })
+ right.on("click", function () {
+    if (offset === 800) {
+      offset = 0;
+      slider.css({
+        "left": 0
+      });
+    } else {
+      offset += photoWidth;
+      slider.css({
+        "left": -offset + 'px'
+      });
+    }
+  });
+
+
+  left.on("click", function () {
+    if (offset === 0) {
+      offset = 800;
+      slider.css({
+        "left": '-800px'
+      });
+    } else {
+      offset -= photoWidth;
+      slider.css({
+        "left": -offset + 'px'
+      });
+    }
+  });
+
+  //scrolling 
+  var link = $("header li a");
+    
+    link.on("click", function (){ 
+    var position = $($(this).attr("href")).position().top; 
+       $("html, body").animate({
+     scrollTop: position 
+  }, 500) 
+    });
+  
+  //showing websites info
+   $(".web-prev").mouseenter(function() {
+     $($(this)).prev(".around").find(".info-prev").css("display", "inline-block");
    });
-  $(".web").mouseleave(function() {
-     $($(this)).next().css("display", "none");
+  $(".web-prev").mouseleave(function() {
+     $($(this)).prev(".around").find(".info-prev").css("display", "none");
+   });
+  
+  $(".web-next").mouseenter(function() {
+     $($(this)).next(".around").find(".info-next").css("display", "inline-block");
+   });
+  $(".web-next").mouseleave(function() {
+     $($(this)).next(".around").find(".info-next").css("display", "none");
    })
    
 });
